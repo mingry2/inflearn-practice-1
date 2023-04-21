08340f9f2061c54e524a310cf6241cb0a044c613
minkyoung kim <minry20415@naver.com>
2023-04-21 19:58:17 +0900

    spring boot + jpa

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 //@Service
+@Transactional
 public class MemberService {
 
 	private final MemberRepository memberRepository;
